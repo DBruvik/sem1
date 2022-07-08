16be0ce4ef47cd8b8d4315d643f6bb18fc3c52dd
Jinot <99970850+Jinot@users.noreply.github.com>
2022-07-08 21:34:20 +0200

    Delete Forelesning 2 - 270821 directory

--- Forelesning 2 - 270821/Forelesning 2 - eget/egentesting31082021-1.py
+++
@@ -1,8 +0,0 @@
-name= input('What is your name? ')
-print ('Your name is', name)
-
-
-
-firstName=input('Enter your first name: ')
-lastName=input('Enter your last name: ')
-print('Hello', firstName, lastName)
